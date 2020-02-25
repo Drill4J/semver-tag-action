a86d32df0c7d5300afa93fe2a14c84a22eb7a808
pavyarov <Pavel_Iarov@epam.com>
2020-02-25 15:43:10 +0300

    added rollup configs

---
+++ rollup.config.js
@@ -0,0 +1,17 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.js',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+  ],
+  plugins: [
+    resolve(),
+    commonjs()
+  ],
+};
