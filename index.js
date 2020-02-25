35a79815edf508d4cfe9a0bef4599ae53e5069de
Viktor Bellavin <viktor_bellavin@epam.com>
2020-02-25 12:18:08 +0300

    added version parsing from git-describe output

--- index.js
+++ index.js
@@ -1,17 +1,30 @@
 const core = require('@actions/core');
 const github = require('@actions/core');
+const exec = require('@actions/exec');
 const semver = require('@drill4j/semver');
+
+const DEFAULT_PRERELEASE_VERSION = semver.parse("0.1.0-0")
 
 async function run() {
     try {
         const type = core.getInput('type');
+        core.info(`Tagging type - ${type}`)
         const token = core.getInput('repo-token');
         if (type === "prerelease") {
-            const version = semver.parse('0.1.0');
+            const describe = await gitDescribe()
+            const version = describe !== '' ? semver.prereleaseFromGit(describe) : DEFAULT_PRERELEASE_VERSION;
+            core.info(`Version - ${version.toString()}`)
+            const client = new github.GitHub(token);
             core.setOutput('tag', version.toString());
         } else if (type === "patch") {
-            const version = semver.parse('0.1.0');
-            core.setOutput('tag', version.toString());
+            const describe = await gitDescribe()
+            if (describe !== '') {
+                const version = semver.patchFromGit(describe);
+                core.info(`Version - ${version.toString()}`)
+                core.setOutput('tag', version.toString());
+            } else {
+                core.setFailed("No version tag found for patch tagging type");
+            }
         } else {
             core.setFailed(`Incorrect tagging type ${type}. Allowed values: "prerelease", "patch"`);
         }
@@ -19,6 +32,25 @@ async function run() {
     catch (error) {
         core.setFailed(error.message);
     }
+}
+
+async function gitDescribe() {
+    let myOutput = '';
+    const options = {
+        listeners: {
+            stdout: (data) => {
+                myOutput += data.toString();
+            }
+        },
+        ignoreReturnCode: true
+    };
+    const args = [
+        'describe',
+        '--tags', '--long',
+        '--match', 'v[0-9]*.[0-9]*.[0-9]*'
+    ]
+    await exec.exec('git', args, options);
+    return myOutput
 }
 
 run()
