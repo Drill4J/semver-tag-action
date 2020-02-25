d1fcb00dd8f44a201387616a71014e1f0a2a37dc
Viktor Bellavin <viktor_bellavin@epam.com>
2020-02-25 11:20:20 +0300

    added logic for "type" input, renamed github-token to repo-token

--- index.js
+++ index.js
@@ -5,10 +5,16 @@ const semver = require('@drill4j/semver');
 async function run() {
     try {
         const type = core.getInput('type');
-        const token = core.getInput('github-token');
-        const version = semver.parse('0.1.0');
-        core.info(`Token ${type}`);
-        core.setOutput('tag', version.toString());
+        const token = core.getInput('repo-token');
+        if (type === "prerelease") {
+            const version = semver.parse('0.1.0');
+            core.setOutput('tag', version.toString());
+        } else if (type === "patch") {
+            const version = semver.parse('0.1.0');
+            core.setOutput('tag', version.toString());
+        } else {
+            core.setFailed(`Incorrect tagging type ${type}. Allowed values: "prerelease", "patch"`);
+        }
     }
     catch (error) {
         core.setFailed(error.message);
